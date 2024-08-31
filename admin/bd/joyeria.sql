c9a689cd03934270405847324d3cbf5bf5583a39
Dell\Cert <isaacpacheco.go@gmail.com>
2024-08-30 21:18:27 -0600

    Se agrega la base de datos

---
+++ admin/bd/joyeria.sql
@@ -0,0 +1,327 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 31-08-2024 a las 05:17:29
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `joyeria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `almacen`
+--
+
+CREATE TABLE `almacen` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `estatus` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `almacen`
+--
+
+INSERT INTO `almacen` (`id`, `nombre`, `estatus`) VALUES
+(1, 'almacen centro', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(35) NOT NULL,
+  `descripcion` varchar(75) NOT NULL,
+  `estatus` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `descripcion`, `estatus`) VALUES
+(1, 'Oro', 'En esta categoría solo guardaremos productos que sean oro solamente', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entradas`
+--
+
+CREATE TABLE `entradas` (
+  `id` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_almacen` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha_registro` date NOT NULL,
+  `hora_registro` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `entradas`
+--
+
+INSERT INTO `entradas` (`id`, `id_producto`, `id_almacen`, `id_usuario`, `cantidad`, `fecha_registro`, `hora_registro`) VALUES
+(1, 1, 0, 3, 1, '2024-08-23', '23:08:40'),
+(2, 1, 0, 3, 11, '2024-08-23', '23:31:40'),
+(3, 1, 1, 3, 1, '2024-08-23', '23:18:51'),
+(4, 1, 1, 3, 1, '2024-08-23', '23:26:53'),
+(5, 2, 1, 3, 10, '2024-08-23', '23:26:53'),
+(6, 1, 1, 3, 1, '2024-08-23', '23:55:59'),
+(7, 2, 1, 3, 10, '2024-08-23', '23:55:59'),
+(8, 1, 1, 3, 50, '2024-08-23', '00:10:04'),
+(9, 1, 1, 3, 100, '2024-08-23', '00:38:04'),
+(10, 1, 1, 3, 10, '2024-08-23', '00:10:05'),
+(11, 2, 1, 3, 5, '2024-08-23', '00:10:05');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `id_unidad` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(75) NOT NULL,
+  `descripcion` varchar(75) NOT NULL,
+  `codigo` varchar(75) NOT NULL,
+  `sku` varchar(75) NOT NULL,
+  `precio_original` double(11,2) NOT NULL,
+  `porcentaje_ganancia` int(11) NOT NULL,
+  `iva` int(11) NOT NULL,
+  `precio_venta` double(11,2) NOT NULL,
+  `fecha_registro` date NOT NULL,
+  `hora_registro` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `id_categoria`, `id_unidad`, `id_usuario`, `nombre`, `descripcion`, `codigo`, `sku`, `precio_original`, `porcentaje_ganancia`, `iva`, `precio_venta`, `fecha_registro`, `hora_registro`) VALUES
+(1, 1, 2, 2, 'anillo con diamantes', 'anillo de oro', 'ANILLO-98891', '75091098387187823', 500.00, 43, 1, 1116.04, '2024-08-13', '23:59:07'),
+(2, 1, 1, 2, 'anillo de plata', 'plata de oro', 'jasdujha7u812', '7456729748727932', 340.00, 5, 1, 414.12, '2024-08-14', '21:30:38');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos_almacen`
+--
+
+CREATE TABLE `productos_almacen` (
+  `id` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_almacen` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `estatus` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos_almacen`
+--
+
+INSERT INTO `productos_almacen` (`id`, `id_producto`, `id_almacen`, `cantidad`, `estatus`) VALUES
+(1, 1, 1, 150, 1),
+(2, 2, 1, 25, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `salidas`
+--
+
+CREATE TABLE `salidas` (
+  `id` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_almacen` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha_registro` date NOT NULL,
+  `hora_registro` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `salidas`
+--
+
+INSERT INTO `salidas` (`id`, `id_producto`, `id_almacen`, `id_usuario`, `cantidad`, `fecha_registro`, `hora_registro`) VALUES
+(1, 1, 1, 0, 10, '2024-08-23', '21:17:51'),
+(2, 2, 1, 0, 5, '2024-08-23', '21:17:51'),
+(3, 1, 1, 0, 8, '2024-08-23', '21:52:51'),
+(4, 2, 1, 0, 5, '2024-08-23', '21:52:51'),
+(5, 0, 1, 0, 11, '2024-08-24', '23:58:13'),
+(6, 0, 1, 0, 1, '2024-08-24', '23:58:13');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `unidades_medida`
+--
+
+CREATE TABLE `unidades_medida` (
+  `id` int(11) NOT NULL,
+  `abreviacion` varchar(15) NOT NULL,
+  `nombre` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `unidades_medida`
+--
+
+INSERT INTO `unidades_medida` (`id`, `abreviacion`, `nombre`) VALUES
+(1, 'KG', 'Kilogramo'),
+(2, 'L', 'Litro');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(75) NOT NULL,
+  `id_almacen` int(11) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nivel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `id_almacen`, `password`, `nivel`) VALUES
+(1, 'issac', 1, '111', 1),
+(2, 'karla', 1, '123', 1),
+(3, 'becky', 1, '111', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `almacen`
+--
+ALTER TABLE `almacen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos_almacen`
+--
+ALTER TABLE `productos_almacen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `salidas`
+--
+ALTER TABLE `salidas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `unidades_medida`
+--
+ALTER TABLE `unidades_medida`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `almacen`
+--
+ALTER TABLE `almacen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `productos_almacen`
+--
+ALTER TABLE `productos_almacen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `salidas`
+--
+ALTER TABLE `salidas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `unidades_medida`
+--
+ALTER TABLE `unidades_medida`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
